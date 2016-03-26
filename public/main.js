25f367bd691da528495c53d6e15c4b1f8184db04
Will Hunt <half-shot@molrams.com>
2016-03-26 16:55:21 +0000

    Random quote code

--- public/main.js
+++ public/main.js
@@ -1,1 +1,12 @@
+function setQuote(){
+  //Experimental request API
+  var rQuotes = new Request("quotes.json");
+  fetch(rQuotes).then(function(response) {
+      return response.json()
+    }).then(function(quotes) {
+      qn = Math.floor(Math.random() * quotes.length);
+      $("header h1").innerHTML = quotes[qn];
+    });
+}
 
+setQuote();
