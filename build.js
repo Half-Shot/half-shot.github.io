bf4aaccbd94da6d6f848af6880f12ded6fef3119
Will Hunt <half-shot@molrams.com>
2016-03-27 15:25:54 +0100

    Moved style to source Build.js now builds sass

--- build.js
+++ build.js
@@ -10,6 +10,21 @@ SOURCEDIR = "./source/"
 console.log("Starting build");
 
 SASS_FILE = path.join(SOURCEDIR,"style","main.scss");
+CSS_FILE = path.join(OUTPUTDIR,"style.css");
+
+console.log("Building SASS")
+
+sass.render({
+  file: SASS_FILE
+}, function(err, result) {
+	if(err){
+		console.error(err);
+	}
+	else
+	{
+		fs.writeFileSync(CSS_FILE,result.css);
+	}
+});
 
 //Build
 console.log("Finished build");
