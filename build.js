1f5b29367bd3a08477dc872e9df97dd24123c820
Will Hunt <half-shot@molrams.com>
2016-03-27 17:13:51 +0100

    Added static files and more moving of functions

--- build.js
+++ build.js
@@ -1,23 +1,34 @@
-const markdown = require('markdown-it');
-const mustache = require('mustache');
 const path = require('path');
 
 //Local functions
 const hs_style = require('./style');
-const hs_markdown = require('./style');
+const hs_pages = require('./pages');
+const ncp = require('ncp').ncp;
 
 OUTPUTDIR = "./public/"
 SOURCEDIR = "./source/"
 
-console.log("Starting build");
-
 SASS_FILE = path.join(SOURCEDIR,"style","main.scss");
 CSS_FILE = path.join(OUTPUTDIR,"style.css");
-
 console.log("Building SASS");
 hs_style.render(SASS_FILE,CSS_FILE);
-console.log("Finished SASS");
 
 
+TEMP_PAGE = path.join(SOURCEDIR,"templates","page.mustache");
+PAGEDIR = path.join(SOURCEDIR,"pages");
+PAGE_OUT = OUTPUTDIR;//We put pages directly in the output dir.
+
+console.log("Building Pages");
+hs_pages.buildPages(PAGEDIR,TEMP_PAGE,PAGE_OUT);
+
+//Copy the static files
+STATIC_FILES = path.join(SOURCEDIR,"rootcontent");
+STATIC_FILES_OUT = path.join(OUTPUTDIR);
+
+ncp(STATIC_FILES,STATIC_FILES_OUT, function (err) {
+ if (err) {
+   throw err;
+ }
+});
 
 console.log("Finished build");
