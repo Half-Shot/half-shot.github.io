7a04165eb8a5c21aed0e38c2374852e1dc6ae6b5
Will Hunt <half-shot@molrams.com>
2016-03-28 14:33:26 +0100

    Added font-awesome

--- build.js
+++ build.js
@@ -8,32 +8,38 @@ const ncp = require('ncp').ncp;
 
 OUTPUTDIR = "./public/"
 SOURCEDIR = "./source/"
+FONTA_FILES = path.join("node_modules","font-awesome");
 
 SASS_FILE = path.join(SOURCEDIR,"style","main.scss");
 CSS_FILE = path.join(OUTPUTDIR,"style.css");
-hs_style.render(SASS_FILE,CSS_FILE);
-
 
 TEMP_PAGE = path.join(SOURCEDIR,"templates","page.mustache");
 PAGEDIR = path.join(SOURCEDIR,"pages");
-PAGE_OUT = OUTPUTDIR;//We put pages directly in the output dir.
 
-hs_pages.buildIndex(PAGEDIR,["blog"]);
-hs_pages.buildPages(PAGEDIR,TEMP_PAGE,PAGE_OUT);
+PAGE_OUT = OUTPUTDIR;//We put pages directly in the output dir.
 
 BLOGDIR = path.join(SOURCEDIR,"blog");
 BLOG_OUT = path.join(OUTPUTDIR,"blog");
 
-hs_blog.buildBlog(BLOGDIR,TEMP_PAGE,TEMP_PAGE,BLOG_OUT)
-
-//Copy the static files
 STATIC_FILES = path.join(SOURCEDIR,"rootcontent");
 STATIC_FILES_OUT = path.join(OUTPUTDIR);
 
+ncp(path.join(FONTA_FILES,"scss"),path.join(SOURCEDIR,"style","font-awesome")); //Copy font scss
+hs_style.render(SASS_FILE,CSS_FILE);
+
+hs_pages.buildIndex(PAGEDIR,["blog"]);
+hs_pages.buildPages(PAGEDIR,TEMP_PAGE,PAGE_OUT);
+
+hs_blog.buildBlog(BLOGDIR,TEMP_PAGE,TEMP_PAGE,BLOG_OUT)
+
+//Copy the static files
 ncp(STATIC_FILES,STATIC_FILES_OUT, function (err) {
  if (err) {
    throw err;
  }
 });
+
+//Copy font awesome
+ncp(path.join(FONTA_FILES,"fonts"),path.join(STATIC_FILES_OUT,"fonts"));
 
 console.log("Finished build");
