07ba1d227fd525272a2b6f501a5b6d90092de4f1
Will Hunt <half-shot@molrams.com>
2016-05-10 18:44:21 +0100

    Moved source files and added sexy new player

---
+++ source/rootcontent/src/player.js
@@ -0,0 +1,33 @@
+var streamURL = "http://half-shot.uk:8000/status-json.xsl";
+var player = document.querySelector("audio#stream_player");
+var streamended = true;
+var player_interval;
+
+function getStream(){
+  var rstatus = new Request(streamURL);
+  fetch(rstatus).then(function(response) {
+    return response.json()
+  }).then(function(streamdata) {
+    var source = streamdata.icestats.source;
+    if(source["audio_bitrate"] != undefined){
+       document.querySelector("#stream_offline").hidden = "true";
+       document.querySelector("#stream_online").hidden = null;
+       document.querySelector("#stream_listeners").innerHTML = source.listeners;
+       if(player.paused && streamended){
+         clearInterval(player_interval);
+         player.src = source.listenurl + "?nocache=" + (Math.random()*100); //Dumb hack
+         player.play();
+       }
+       streamended = false;
+    }
+    else
+    {
+       document.querySelector("#stream_offline").hidden = null;
+       document.querySelector("#stream_online").hidden = "true";
+       streamended = true;
+    }
+  });
+}
+
+player.addEventListener("ended", function() { player_interval = setInterval(getStream,5000); }, true);
+player_interval = setInterval(getStream,5000);
