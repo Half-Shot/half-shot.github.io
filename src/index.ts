ec777f610720789cd73fe1417957fb4f5ef07296
Half-Shot <will@half-shot.uk>
2020-06-26 20:02:21 +0100

    Add support for scss

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import { readConfig, AppConfig } from "./config";
 import { renderPage } from "./PageRender";
-import { promises as fs,  watch } from "fs";
+import { promises as fs,  watch, writeFile, writeFileSync } from "fs";
 import * as path from "path";
 import { createWriteStream } from "fs";
 import express from "express";
+import sass from "sass";
+import { promisify } from "util";
+
+const renderSass = promisify(sass.render);
 
 const CONTENT_DIR = path.resolve(__dirname, "..", "content");
+const SCSS_DIR = path.join(CONTENT_DIR, "style");
+const ROOT_SASS_DOC = path.join(SCSS_DIR, "main.scss");
 const PAGES_IN_DIR = path.join(CONTENT_DIR, "pages");
 const PAGES_OUT_DIR = path.resolve(__dirname, "..", "public");
+const CSS_DOC = path.join(PAGES_OUT_DIR, "main.css");
 
 async function buildSite() {
     const config = await readConfig("./site.toml");
     const pages = await fs.readdir(PAGES_IN_DIR, { withFileTypes: true });
+    writeFileSync(CSS_DOC, (await renderSass({ file: ROOT_SASS_DOC })).css);
     for (const dirEntry of pages) {
         if (!dirEntry.isFile()) {
             continue;
@@ -50,6 +58,10 @@ async function watchSite() {
         } catch (ex) {
             console.log(`Error building: ${fileName}`, ex);
         }
+    });
+    watch(SCSS_DIR, { recursive: true, }, async (eventType: string, fileName: string) => {
+        console.log(eventType, fileName);
+        writeFileSync(CSS_DOC, (await renderSass({ file: ROOT_SASS_DOC })).css);
     });
 }
 
